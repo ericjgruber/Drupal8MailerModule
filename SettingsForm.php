15f5e597b805a091dd27e530e302c2b384575c2e
Eric J. Gruber <ericjgruber@users.noreply.github.com>
2019-07-16 13:46:06 -0500

    Add files via upload

---
+++ SettingsForm.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Drupal\mailer\Form;
+
+use Drupal\Core\Form\ConfigFormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Configures mailer notification settings for this site.
+ */
+class SettingsForm extends ConfigFormBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'mailer_settings';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getEditableConfigNames() {
+    return ['mailer.settings'];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $form['no_reply_email'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('No-Reply Email Address'),
+      '#description' => $this->t('Reply email address to use no-reply emails'),
+      '#default_value' => $this->config('mailer.settings')->get('no_reply_email'),
+    ];
+    $form['notification_email'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Notification Email Address'),
+      '#description' => $this->t('Email Address to send notifications to.'),
+      '#default_value' => $this->config('mailer.settings')->get('notification_email'),
+    ];
+
+    return parent::buildForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+    parent::validateForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $values = $form_state->getValues();
+
+    $this->config('mailer.settings')
+      ->set('no_reply_email', $values['no_reply_email'])
+      ->set('notification_email', $values['notification_email'])
+      ->save();
+
+    parent::submitForm($form, $form_state);
+  }
+
+}
